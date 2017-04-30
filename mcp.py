42fcc64fc551eb288ae510d999a4f57838f9a9a9
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-05-01 00:37:08 +0200

    With Logo and DB

--- mcp.py
+++ mcp.py
@@ -1,20 +1,17 @@
 import picamera
 import os
-import numpy as np
 import cv2
 import sys
 import uuid
 import time
-import types
 import random
 import json
-import requests
 import ConfigParser
-import RPi.GPIO       as GPIO
-import cognitive_face as CF
-from subprocess import call
-from watson_developer_cloud import VisualRecognitionV3
-from cloudant.client import Cloudant
+import RPi.GPIO               as     GPIO
+import cognitive_face         as     CF
+from   subprocess             import call
+from   watson_developer_cloud import VisualRecognitionV3
+from   cloudant.client        import Cloudant
 
 class PHP():
 
@@ -31,7 +28,13 @@ class PHP():
         CF.Key.set(self._config.get("MICROSOFT", "FaceKey"))
         self._visual_recognition = VisualRecognitionV3('2016-05-20', api_key=self._config.get("IBM", "visualkey"))
         self._camera = picamera.PiCamera()
-        #self._db = Cloudant(self._config.get("CLOUDANT", "Username"), self._config.get("CLOUDANT", "Password"), account=self._config.get("CLOUDANT", "Host"), connect=True)
+        if not os.path.isdir(self._config.get("DIRS", "Capture")):
+            print "Creating Capture Dir"
+            os.makedirs(self._config.get("DIRS", "Capture"))
+        if not os.path.isdir(self._config.get("DIRS", "Mosaic")):
+            print "Creating Mosaic Dir"
+            os.makedirs(self._config.get("DIRS", "Mosaic"))
+        self._db = Cloudant(self._config.get("CLOUDANT", "Username"), self._config.get("CLOUDANT", "Password"), url=self._config.get("CLOUDANT", "Host"), connect=True)
 
     def _generate_game_goals(self):
         self._game_goals = {}
@@ -64,17 +67,11 @@ class PHP():
             print "No Capture Directory"
             update = True
             self._config.set("DIRS", "Capture", "/tmp/php/captures")
-            if not os.path.isdir(self._config.get("DIRS", "Capture")):
-                print "Creating Capture Dir"
-                os.makedirs(self._config.get("DIRS", "Capture"))
 
         if not self._config.has_option("DIRS", "Mosaic"):
             print "No Mosaic Directory"
             update = True
             self._config.set("DIRS", "Mosaic", "/tmp/php/mosaic")
-            if not os.path.isdir(self._config.get("DIRS", "Mosaic")):
-                print "Creating Mosaic Dir"
-                os.makedirs(self._config.get("DIRS", "Mosaic"))
 
         if not self._config.has_section('CLOUDANT'):
             print "Adding Cloudant part"
@@ -132,7 +129,7 @@ class PHP():
     def _get_ibm_results(self):
         with open(self._current_picture_name, 'rb') as image_file:
             #self._ibm_face_results = self._visual_recognition.detect_faces(images_file=image_file)
-            self._ibm_results = self._visual_recognition.classify(images_file=image_file)
+            self._ibm_result = self._visual_recognition.classify(images_file=image_file)
 
     def _store_in_db(self):
         if "ms_results" not in self._db.all_dbs():
@@ -145,20 +142,28 @@ class PHP():
 
         if self._ms_result is not None:
             ms_database = self._db['ms_results']
-            ms_document = ms_database.create_document(self._ms_result)
+            j = {}
+            j['result']     = self._ms_result
+            j['image_name'] = self._current_picture_name
+            ms_document = ms_database.create_document(j)
 
             if ms_document.exists():
                 print 'SUCCESS MS!!'
 
-        if self._ibm_results is not None:
+        if self._ibm_result is not None:
             ibm_database = self._db['ibm_results']
-            ibm_document = ibm_database.create_document(self._ibm_results)
+            j = {}
+            j['result']     = self._ibm_result
+            j['image_name'] = self._current_picture_name
+            ibm_document = ibm_database.create_document(j)
 
             if ibm_document.exists():
                 print 'SUCCESS IBM!!'
 
     def _enhance_image(self):
-        img = cv2.imread(self._current_picture_name)
+        img   = cv2.imread(self._current_picture_name)
+        logo  = cv2.imread("ibm-bluemix.png")
+        slogo = cv2.resize(logo, None, fx=0.26, fy=0.26, interpolation=cv2.INTER_CUBIC)
 
         for currFace in self._ms_result:
             faceRectangle = currFace['faceRectangle']
@@ -171,15 +176,26 @@ class PHP():
                                color=(255, 255, 0), thickness=3)
             cv2.putText(img, textToWrite, (faceRectangle['left'], faceRectangle['top'] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 1)
 
-        cv2.rectangle(img, (0, 0), (1400, 60), color=(0, 0, 0), thickness=60)
+        cv2.rectangle(img, (0, 0), (1670, 60), color=(0, 0, 0), thickness=60)
         textToWrite = "Goal   : Age:%03.1f / Male:%d / Female:%d / Glases:%d / Beard:%d / Happy:%d / Surprised:%d" % (self._game_goals['age'], self._game_goals['male'], self._game_goals['female'], self._game_goals['glasses'], self._game_goals['hair'], self._game_goals['happy'], self._game_goals['surprised'])
-        cv2.putText(img, textToWrite, (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (  0, 255,   0), 3)
+        cv2.putText(img, textToWrite, (270, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (  0, 255,   0), 3)
         textToWrite = "Current: Age:%03.1f / Male:%d / Female:%d / Glases:%d / Beard:%d / Happy:%d / Surprised:%d" % (self._game_results['age'], self._game_results['male'], self._game_results['female'], self._game_results['glasses'], self._game_results['hair'], self._game_results['happy'], self._game_results['surprised'])
-        cv2.putText(img, textToWrite, (0, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (  0,   0, 255), 3)
+        cv2.putText(img, textToWrite, (270, 80), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (  0,   0, 255), 3)
         #if self._check_game():
         #    cv2.putText(img, "You Won!", (0, 120), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 3)
         #else:
         #    cv2.putText(img, "Find more people :-)", (0, 120), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (  0, 0, 255), 3)
+
+        rows, cols, channels  = slogo.shape
+        roi                   = img[0:rows, 0:cols]
+        img2gray              = cv2.cvtColor(slogo, cv2.COLOR_BGR2GRAY)
+        ret, mask             = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
+        mask_inv              = cv2.bitwise_not(mask)
+        img1_bg               = cv2.bitwise_and(roi, roi, mask=mask_inv)
+        img2_fg               = cv2.bitwise_and(slogo, slogo, mask=mask)
+        dst                   = cv2.add(img1_bg, img2_fg)
+        img[0:rows, 0:cols]  = dst
+
         cv2.imwrite('/tmp/test.png', img)
 
     def _cut_faces(self):
@@ -273,11 +289,12 @@ class PHP():
                 self._extract_game_results()
                 self._enhance_image()
                 GPIO.output(12, False)
-                print json.dumps(self._ms_result,   sort_keys=True, indent=4, separators=(',', ': '))
-                print json.dumps(self._ibm_results, sort_keys=True, indent=4, separators=(',', ': '))
+                print json.dumps(self._ms_result,  sort_keys=True, indent=4, separators=(',', ': '))
+                print json.dumps(self._ibm_result, sort_keys=True, indent=4, separators=(',', ': '))
                 print self._game_goals
                 print self._game_results
                 call(["killall", "fbi"])
+                self._store_in_db()
 
 if __name__ == "__main__":
     p = PHP()
